8656f5ef6f8e311cf077620535abc100ba1b54d5
Theo Roque <theoroque95@gmail.com>
2023-09-11 09:22:07 +0800

    Fix build issue

--- apps/app/app/auth/signup/route.ts
+++ apps/app/app/auth/signup/route.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@/utils/supabase-api'
 import { NextRequest, NextResponse } from 'next/server'
 
-export const dynamic = 'force-dynamic'
-
 export async function POST(req: NextRequest) {
   try {
     const { email, password } = await req.json()
